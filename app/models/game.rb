b8c977087b2efabae56f88c541bf9c7bd8dafcf8
Steven Spiel <stevenspiel@gmail.com>
2014-07-23 18:33:45 -0400

    add quick destroy commands

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ class Game < ActiveRecord::Base
 
   has_many   :players,   dependent:  :destroy
   has_many   :blocks,    dependent:  :destroy
-  has_many   :moves,     dependent:  :destroy
+  has_many   :actions,   dependent:  :destroy
   has_one    :winner,    class_name: 'User',   foreign_key: "winner_id"
   belongs_to :turn,      class_name: 'Player', foreign_key: "turn_id"
   belongs_to :initiator, class_name: 'User',   foreign_key: "initiator_id"
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ActiveRecord::Base
   has_many :games
 
   scope :available, -> { where(available: true) }
+
+  def self.delete
+    User.all.each(&:destroy)
+  end
+
+  def self.delete_everything
+    [User,Game,Action,Block,Player].each do |model|
+      model.all.each(&:destroy)
+    end
+  end
   
 end
