c836306f338b572cf7adec24eff08353ffe37a3b
Steven Spiel <stevenspiel@gmail.com>
2014-08-04 07:14:06 -0400

    change oauth variable for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
+google_secret = Rails.env.production? ? ENV["GOOGLE_CLIENT_SECRET_PRODUCTION"] : ENV["GOOGLE_CLIENT_SECRET"]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], google_secret
 end
