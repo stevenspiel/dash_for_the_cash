7abc5e45f34c56f79489c35efb3153e145fc2b07
Steven Spiel <stevenspiel@gmail.com>
2014-08-04 08:20:30 -0400

    revert google client secret change

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
-google_secret = Rails.env.production? ? ENV["GOOGLE_CLIENT_SECRET_PRODUCTION"] : ENV["GOOGLE_CLIENT_SECRET"]
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], google_secret
+  provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
