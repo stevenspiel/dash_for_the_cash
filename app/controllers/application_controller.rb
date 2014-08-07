e0309211b7ae0784585b192a2579593a8aba4547
Steven Spiel <stevenspiel@gmail.com>
2014-08-07 18:57:22 -0400

    no message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   before_filter :require_login
+  config.serve_static_assets = true
 
   def valid_session_id
     User.find_by(id: session[:user_id]) if session[:user_id]
